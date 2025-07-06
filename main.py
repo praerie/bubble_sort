db4404763f6457d28119e8e05564f90eba939f80
Adelie <elisabethadelie@gmail.com>
2025-07-06 14:44:12 -0400

    run merge and heap from main

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from utils.dataset_utils import load_dataset
 from utils.benchmark_utils import time_sorting
 from algorithms.bubble_sort import bubble_sort
+from algorithms.merge_sort import merge_sort
+from algorithms.heap_sort import heap_sort
 import os
 
 def main():
     filenames = sorted(os.listdir("datasets"))
+
+    # bubble sort 
     for file in filenames:
         data = load_dataset(file)
         duration = time_sorting(bubble_sort, data.copy())
+        print(f"{file:<25} | Time: {duration:.2f} ms")
+
+    # merge sort
+    for file in filenames:
+        data = load_dataset(file)
+        duration = time_sorting(merge_sort, data.copy())
+        print(f"{file:<25} | Time: {duration:.2f} ms")
+
+    # heap sort
+    for file in filenames:
+        data = load_dataset(file)
+        duration = time_sorting(heap_sort, data.copy())
         print(f"{file:<25} | Time: {duration:.2f} ms")
 
 if __name__ == "__main__":
